0929677d6979048b4d141d0458c119951ec91b95
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-12-06 23:24:47 +0000

    update

--- volume_of_cylinder.py
+++ volume_of_cylinder.py
@@ -8,31 +8,36 @@
 import math
 
 
-def calculate_volume(radius: int, height: int) -> None:
-    # calculate volume
+def calculate_cylinder_volume(height: int, radius: int) -> float:
+    # This function calculates volume
 
-    # process
-    volume = math.pi * (radius * radius) * height
-
-    # output
-    print("The volume of the cylinder is {0} cm³".format(volume))
+    if height <= 0 or radius <= 0:
+        volume = float(-1)
+        return volume
+    else:
+        volume = float(math.pi * (radius * radius) * height)
+        return volume
 
 
 def main():
-    # this function gets the base and height
+    # This function gets the volume
 
-    # input
-    radius_from_user = int(input("Enter the radius value of the cylinder (cm): "))
-    height_from_user = int(input("Enter the height value of the cylinder (cm): "))
-    print("")
+    # Input
+    height_from_user = input("Enter the height of the cylinder(mm): ")
+    radius_from_user = input("Enter the radius of the cylinder(mm): ")
 
     try:
-        radius = float(radius_from_user)
-        height = float(height_from_user)
-        if radius_from_user and height_from_user < 0:
-            print("\n-1cm³")
+        height_from_user = float(height_from_user)
+        radius_from_user = float(radius_from_user)
+        # call functions
+        final_volume = calculate_cylinder_volume(height_from_user, radius_from_user)
+        if final_volume < 0:
+            print("-1")
+        else:
+            print("The volume of this cylinder is {0} mm³.".format(final_volume))
     except ValueError:
-        print("\nInvalid Input")
+        print("Invalid Input")
+
     print("\nDone.")
 
 
